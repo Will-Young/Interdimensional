1821a350e1744b5f6c408be148b71bfd68a770d2
Will <will.c.young@hotmail.com>
2023-01-12 22:12:11 +1100

    More work on the upgrade system

---
+++ Assets/Scripts/ScriptableObjects/StampingCatUpgradeSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName ="StampingCatUpgrade")]
+public class StampingCatUpgradeSO : ScriptableObject
+{
+    public string Name;
+    
+}
